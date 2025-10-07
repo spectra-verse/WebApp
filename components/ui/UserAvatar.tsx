1dcfaa3480de31723970307a279d420dc97723f1
Karan Dhillon <Karanpal.dhillon@gmail.com>
2025-10-07 18:10:09 +0530

    sidebar

--- components/ui/UserAvatar.tsx
+++ components/ui/UserAvatar.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import { SHA256 } from "crypto-js";
+"use client";
+import { MD5 } from "crypto-js";
 
 const UserAvatar = ({
   email,
@@ -13,14 +13,14 @@ const UserAvatar = ({
   // Gravatar requires the email to be trimmed and lowercased [1.2.3]
   const processedEmail = email.trim().toLowerCase();
 
-  // Hash the email with SHA256 as recommended [1.2.3, 1.4.6]
-  const hash = SHA256(processedEmail).toString();
+  // Hash the email with MD5 as required by Gravatar API
+  const hash = MD5(processedEmail).toString();
 
   // Construct the Gravatar URL [1.1.2, 1.2.1]
   const gravatarUrl = `https://www.gravatar.com/avatar/${hash}?s=${size}&d=mp`;
 
   return (
-    <Image
+    <img
       src={gravatarUrl}
       width={size}
       height={size}
